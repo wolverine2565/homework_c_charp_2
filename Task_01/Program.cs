7df6734a87e886c8e7a65651878d6f014fb0a7b4
email <wolverine89@bk.ru>
2022-11-26 23:58:02 +0300

    Task_01

---
+++ Task_01/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите число");
+
+var a = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+
+while (a >= 100)
+{
+    a /= 10;
+}
+
+var b = a % 10;
+
+Console.WriteLine("Вторая цифра " + b);
