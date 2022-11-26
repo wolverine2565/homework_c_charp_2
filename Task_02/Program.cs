313d173deed860adb0b169b7885885d3fbff569d
email <wolverine89@bk.ru>
2022-11-27 00:21:46 +0300

    Task_02

---
+++ Task_02/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Random rnd = new Random();
+ 
+int a = rnd.Next(100, 999);
+ 
+Console.WriteLine(a); 
+
+int b = int.Parse(a.ToString().Remove(1,1));
+Console.WriteLine(a + " -> " + b);
