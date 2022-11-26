0727110259a8ddc1840b232653b62b7f74f8da2a
email <wolverine89@bk.ru>
2022-11-27 01:51:54 +0300

    Task_03

---
+++ Task_03/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+/*Задача 3:
+Напишите программу, которая выводит третью цифру
+заданного числа или сообщает, что третьей цифры нет.*/
+
+Console.WriteLine("Введите число");
+
+int a = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+
+if(a < 100)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+
+else
+Console.WriteLine(a + " -> " + (a.ToString()[2]));
