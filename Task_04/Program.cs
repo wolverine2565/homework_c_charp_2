a19c8ad7597bcd2a072d31ff4fd67bae6f0f0128
email <wolverine89@bk.ru>
2022-11-27 03:51:11 +0300

    Task_43

---
+++ Task_04/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+/*Задача 4:
+Напишите программу, которая принимает на вход цифру,
+обозначающую день недели, и проверяет, является ли этот день выходным.*/
+
+
+
+
+Console.WriteLine("Введите число");
+int a = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+
+if ((a>0) && (a<=5))
+{
+    Console.WriteLine("Рабочий день");
+}
+else 
+    if ((a == 6) ||  (a == 7))
+    {
+        Console.WriteLine("Выходной день");
+    }
+else 
+Console.WriteLine("Неверное значение");
